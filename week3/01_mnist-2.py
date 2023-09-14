d7121890e428e975c7cd5c976a9206b2fa906b2c
NLPlab-skku <nlplab.skku@gmail.com>
2023-09-14 19:35:06 +0900

    Add files via upload

---
+++ week3/01_mnist-2.py
@@ -0,0 +1,55 @@
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import fetch_openml
+mnist = fetch_openml('mnist_784', version=1, parser = 'auto', as_frame=False)
+#print(mnist.keys())
+
+X, y = mnist["data"], mnist["target"]
+#print(X.shape, y.shape)
+
+#integer로 타입을 바꾸기
+y = y.astype(np.uint8)
+#print(y)
+
+#학습 및 테스트 데이터 분리
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[:60000]
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+#print(sgd_clf.predict([some_digit])
+
+#Cross Validation 
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+
+from sklearn.model_selection import cross_val_score
+print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+#confusion matrix
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_train_5, y_train_pred))
