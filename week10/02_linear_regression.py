fec68e1078390da5598d581f7def31edea0c7cec
HACore <lhe5254@gmail.com>
2023-11-09 18:24:46 +0900

    Fix files

--- week10/02_linear_regression.py
+++ week10/02_linear_regression.py
@@ -4,21 +4,22 @@ import numpy as np
 
 X = 2 * np.random.rand(100,1)
 y = 4 + 3 * X + np.random.randn(100, 1) # y = 4 + 3x + Gaussian noise
+print(X)
+input()
 
-#print(X)
-
-X_b = np.c_[np.ones((100,1)),X] #1을 더하는 이유는 x0(바이어스)
-print(X_b)
+print(y)
 input()
 
 X_new = np.array([[0],[2]])
-#print(X_new)
+print(X_new)
+input()
 
 from sklearn.linear_model import LinearRegression
 Lin_reg = LinearRegression()
 Lin_reg.fit(X,y)
 
 print(Lin_reg.intercept_, Lin_reg.coef_)
+input()
 print(Lin_reg.predict(X_new))
 input()
 
@@ -27,11 +28,17 @@ eta = 0.1 # learning rate
 n_iterations = 100
 m = 100
 
+X_b = np.c_[np.ones((100,1)),X] #1을 더하는 이유는 x0(바이어스)
+print(X_b)
+input()
+
 theta = np.random.randn(2,1)
+print("before",theta)
+input()
 
 for iteration in range(n_iterations):
     gradients = 2/m * X_b.T.dot(X_b.dot(theta)-y)
     theta = theta -eta * gradients
 
-print(theta)
+print("after", theta)
 
